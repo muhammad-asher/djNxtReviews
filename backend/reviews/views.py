691d59965b94d739df780de51a5a0493f218272c
muhammad-asher <muhammadasher.se@gmail.com>
2023-10-13 20:37:22 +0500

    Create reviews , business and category viewSets

--- backend/reviews/views.py
+++ backend/reviews/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
 from . import serializers
-
+from . import models
 
 # Create your views here.
 
@@ -16,3 +16,22 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = serializers.GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class BusinessViewSet(viewsets.ModelViewSet):
+    queryset = models.Business.objects.all()
+    serializer_class = serializers.BusinessSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = models.Category.objects.all()
+    serializer_class = serializers.CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
